649599633e09c0a373f7836137012d5cef7e0efa
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2015-08-07 00:49:21 -0400

    changing assembly information

--- PrettyCash/Properties/AssemblyInfo.cs
+++ PrettyCash/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PrettyCash")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("PrettyCash")]
 [assembly: AssemblyProduct("PrettyCash")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © PrettyCash 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
